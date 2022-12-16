5f5cf8399ae8a182fb889c805423c062cfaffa82
Junyi Zheng <zhengjunyi@unm.edu>
2022-12-16 13:49:08 +0000

    Upload New File

---
+++ src/Auction/Item.java
@@ -0,0 +1,59 @@
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Item {
+	private String name;
+	private String price;
+	private String description;
+	public String currentBid;
+	public ItemBider currentHolder;
+	public Timer timer;
+	public boolean soldOut = false;
+	public boolean soldIng = false;
+	public void timerStart() {
+		soldIng = true;
+		timer = new Timer("timerStart");
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				// 定时，时间到发给出价最高的即可
+				String outbidMessage = "replace\t" + "win\t" + "replace\t" + "replace\t" + getName() + "\t"
+						+ currentBid + "\t";
+				currentHolder.agentHandler.send(outbidMessage);
+				soldOut = true;
+			}
+		}, 30000);
+	}
+
+	public Item(String n, String p) {
+		name = n;
+		price = p;
+		description = "here is description template";
+		currentBid = price;
+		currentHolder = null;
+	}
+
+	public String myToString() {
+		return name + "\t" + description + "\t" + price + "\t" + currentBid + "\t";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
